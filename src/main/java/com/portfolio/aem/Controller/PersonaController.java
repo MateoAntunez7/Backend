f4b717b5066e6e9f3cf786ce1081393e3d12e7d9
Mateo Antunez <mateoantunez0@gmail.com>
2023-03-07 21:53:19 -0300

    fix backend

--- src/main/java/com/portfolio/aem/Controller/PersonaController.java
+++ src/main/java/com/portfolio/aem/Controller/PersonaController.java
@@ -5,16 +5,21 @@ import com.portfolio.aem.Entity.Persona;
 import com.portfolio.aem.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"https://frontendaem.web.app/","http://localhost:4200"})
+@RequestMapping("/personas")
+
 public class PersonaController{
     @Autowired IPersonaService ipersonaService;
     
